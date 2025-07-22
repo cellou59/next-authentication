5242777469aee86d67a8d40c03ede4be7d6d2a3c
cellouRetailShake <cellu@retailshake.com>
2025-07-22 05:12:48 +0200

    🐛 fix: Implement user validation and password hashing in signUp and signIn functions; add a new user to the database

--- src/app/exercises/auth/lib/auth.exercice.ts
+++ src/app/exercises/auth/lib/auth.exercice.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {RoleEnum} from '@/lib/type'
 // 🐶 Importe `bcrypt` from `'bcrypt'`
-// import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt'
 
 // 🐶 Importe `bcrypt` from 'addUser' 'getUserByEmail'
-// import {addUser, getUserByEmail} from '@/db/sgbd'
-// import {SignInError} from './type'
+import {addUser, getUserByEmail} from '@/db/sgbd'
+import {SignInError} from './type'
 
 const signUp = async (email: string, password: string) => {
   await new Promise((resolve) => setTimeout(resolve, 1000))
@@ -13,15 +13,21 @@ const signUp = async (email: string, password: string) => {
 
   // 🐶 1. Vérification de l'utilisateur en BDD
   // Lève une erreur si l'utilisateur existe déjà
-  // 🤖 const user = await getUserByEmail(email)
+  const user = await getUserByEmail(email)
+  if (user) {
+    throw {
+      type: 'CredentialsSignin',
+      message: 'Invalid User.',
+    } as SignInError
+  }
 
   // 🐶 2. Hachage du mot de passe
   // https://github.com/kelektiv/node.bcrypt.js?tab=readme-ov-file#to-hash-a-password
   const saltRounds = 10
-  const salt = 'salt' // 🤖 await bcrypt.genSalt(saltRounds)
+  const salt = await bcrypt.genSalt(saltRounds)
 
   // Hachage du mot de passe avec le salt
-  const hashedPassword = 'hashedPassword' // 🤖 await bcrypt.hash(password, salt  )
+  const hashedPassword = await bcrypt.hash(password, salt)
 
   const newUser = {
     email,
@@ -30,10 +36,16 @@ const signUp = async (email: string, password: string) => {
     role: RoleEnum.USER,
   }
   // 🐶 3. Ajout de l'utilisateur en BDD
-  // 🤖 const createdUser = await addUser(newUser)
+  const createdUser = await addUser(newUser)
+  if (!createdUser) {
+    throw {
+      type: 'CredentialsSignin',
+      message: 'add user error',
+    } as SignInError
+  }
 
   // 🐶 4. Retourne l'utilisateur créé
-  return {email, role: RoleEnum.USER}
+  return {...createdUser, role: RoleEnum.USER}
 }
 
 const signIn = async (email: string, password: string) => {
@@ -41,24 +53,29 @@ const signIn = async (email: string, password: string) => {
   console.log('signIn ...', email, password)
 
   // 🐶 1. Vérification de l'utilisateur en BDD
-  // 🤖 getUserByEmail
+  const user = await getUserByEmail(email)
 
   // Lève une erreur si l'utilisateur n'existe pas
-  // 🤖
-  // if (!user) {
-  //   // eslint-disable-next-line no-throw-literal
-  //   throw {
-  //     type: 'CredentialsSignin',
-  //     message: 'Invalid User.',
-  //   } as SignInError
-  // }
+  if (!user) {
+    throw {
+      type: 'CredentialsSignin',
+      message: 'Invalid User.',
+    } as SignInError
+  }
 
   // 🐶 2. Comparaison du mot de passe
-  // 🤖 bcrypt.compare
+  const match = await bcrypt.compare(password, user.password)
+
+  if (!match) {
+    throw {
+      type: 'CredentialsSignin',
+      message: 'Invalid password.',
+    } as SignInError
+  }
 
   // Lève une erreur si le mot de passe ne correspond pas : message: `Invalid credentials.`
   // 🐶 Retourne le user de BDD.
-  return {email, role: RoleEnum.USER}
+  return {user, role: RoleEnum.USER}
 }
 
 async function logout() {
