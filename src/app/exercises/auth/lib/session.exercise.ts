94db076161a275aa342fe2458b232c6fc5b4b78d
cellouRetailShake <cellu@retailshake.com>
2025-07-24 07:03:07 +0200

    💡 refactor: Implement session management using Strategy Pattern for different session types

--- src/app/exercises/auth/lib/session.exercise.ts
+++ src/app/exercises/auth/lib/session.exercise.ts
@@ -1,16 +1,13 @@
 /* eslint-disable no-restricted-imports */
+import {SessionType} from './type'
 
-// 🐶 Importe le type `SessionType`
-// 🤖 import {SessionType} from './type'
+import {
+  createSession as createSessionDb,
+  deleteSession as deleteSessionDb,
+  verifySession as verifySessionDb,
+  updateSession as updateSessionDb,
+} from './session-database'
 
-// 🐶 Importe les fonctions de session database
-// 🤖
-// import {
-//   createSession as createSessionDb,
-//   deleteSession as deleteSessionDb,
-//   verifySession as verifySessionDb,
-//   updateSession as updateSessionDb,
-// } from './session-database'
 import {
   createSession as createSessionStateLess,
   deleteSession as deleteSessionStateLess,
@@ -19,29 +16,64 @@ import {
 } from './session-stateless'
 
 // 🐶 Récupère le type de session définie en variable d'environnement
-// 🤖 const SESSION_TYPE: SessionType = process.env.SESSION_TYPE as SessionType
+const SESSION_TYPE: SessionType = process.env.SESSION_TYPE as SessionType
 
 // 🐶 Implémente le `Strategy Pattern`
 export async function createSession(userId: string) {
-  // 🐶 Crée une session en fonction du type de session définie
-  // Tu peux utiliser un `switch case` pour appeler la bonne fonction
-  // 🤖 switch (SESSION_TYPE) {
-  return await createSessionStateLess(userId)
-  // 🐶 Lève une erreur en cas de type invalide
-  // 🤖 throw new Error('Invalid session type')
+  switch (SESSION_TYPE) {
+    case SessionType.STATELESS: {
+      return await createSessionStateLess(userId)
+    }
+    case SessionType.DATABASE: {
+      return await createSessionDb(userId)
+    }
+    default: {
+      throw new Error('Invalid session type')
+    }
+  }
 }
 
 // 🐶 Implémente le `Strategy Pattern`
 export async function verifySession() {
-  return await verifySessionStateless()
+  switch (SESSION_TYPE) {
+    case SessionType.STATELESS: {
+      return await verifySessionStateless()
+    }
+    case SessionType.DATABASE: {
+      return await verifySessionDb()
+    }
+    default: {
+      throw new Error('Invalid session type')
+    }
+  }
 }
 
 // 🐶 Implémente le `Strategy Pattern`
 export async function updateSession() {
-  return await updateSessionStateLess()
+  switch (SESSION_TYPE) {
+    case SessionType.STATELESS: {
+      return await updateSessionStateLess()
+    }
+    case SessionType.DATABASE: {
+      return await updateSessionDb()
+    }
+    default: {
+      throw new Error('Invalid session type')
+    }
+  }
 }
 
 // 🐶 Implémente le `Strategy Pattern`
-export function deleteSession() {
-  return deleteSessionStateLess()
+export async function deleteSession() {
+  switch (SESSION_TYPE) {
+    case SessionType.STATELESS: {
+      return await deleteSessionStateLess()
+    }
+    case SessionType.DATABASE: {
+      return await deleteSessionDb()
+    }
+    default: {
+      throw new Error('Invalid session type')
+    }
+  }
 }
