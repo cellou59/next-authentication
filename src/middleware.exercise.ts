a0f4e9607d1e12d957363f9c793e67ed5fa4407e
cellouRetailShake <cellu@retailshake.com>
2025-07-24 07:54:09 +0200

    💡 refactor: Simplify route handling and session management in middleware

--- src/middleware.exercise.ts
+++ src/middleware.exercise.ts
@@ -1,18 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {NextResponse, type NextRequest} from 'next/server'
 //🐶 Importe `Cookies`
-//🤖 import {cookies} from 'next/headers'
+import {cookies} from 'next/headers'
+import {decrypt} from './app/exercises/auth/lib/crypt'
 
 //🐶 1. Définies les routes privées et publiques dans un `Set`
 
-// 🤖 const protectedRoutes
-// Privés
-// '/exercises/dashboard',
-// '/exercises/bank-account',
+const protectedRoutes = new Set([
+  '/exercises/dashboard',
+  '/exercises/bank-account',
+])
 
-// 🤖 const publicRoutes
-// Publics
-// '/',
+const publicRoutes = new Set(['/'])
 
 export async function middleware(request: NextRequest) {
   //🐶 2. Vérifie si la route courante est privée ou publique
@@ -20,19 +19,25 @@ export async function middleware(request: NextRequest) {
 
   // 🐶 Base toi sur `path`, `protectedRoutes` et `publicRoutes`
   // pour determiner les 2 constantes ci-desous
-  const isProtectedRoute = false
-  const isPublicRoute = true
+  const isProtectedRoute = protectedRoutes.has(path)
+  const isPublicRoute = publicRoutes.has(path)
 
   // 🐶 3. Récupère le cookie de session
-  // 🤖 const cookieStore = await cookies()
-  // const cookie = cookieStore.get('session')?.value
-  // 🤖 const session = await decrypt(cookie)
+  const cookieStore = await cookies()
+  const cookie = cookieStore.get('session')?.value
+  const session = await decrypt(cookie)
 
   // 🐶 4. Vérifie si l'utilisateur a une session (session?.userId || session?.sessionId)
-  const hasSession = false
+  const hasSession = session?.userId || session?.sessionId
 
   // 🐶 5. Redirige vers `/exercises/login` si la route est privée et qu'il n'y a pas de session
   // 📑 https://nextjs.org/docs/app/building-your-application/routing/redirecting#nextresponseredirect-in-middleware
+  if (isProtectedRoute && !hasSession) {
+    return NextResponse.redirect(new URL('/exercises/login', request.url))
+  }
+  if (isPublicRoute && hasSession) {
+    return NextResponse.redirect(new URL('/exercises/auth', request.url))
+  }
 
   // 🐶 6. Redirige vers `/exercises/auth` si la route est publique et qu'il y a une session
 
